43870f15b026c6f00688b591c12805110f610dbc
dorigum <41736468+dorigum@users.noreply.github.com>
2026-02-04 01:28:34 +0900

    난수 형태로 수정중 -> 다시 풀어보기

--- Java/work/step01_Basic/src/ex0203_구도연/Exercise01.java
+++ Java/work/step01_Basic/src/ex0203_구도연/Exercise01.java
@@ -4,23 +4,33 @@ public class Exercise01 {
 
 	public static void main(String[] args) {
 		// 성적표 만들기
-		// 이름(String), 국어 점수(int), 영어 점수(int), 수학 점수(int) 변수에 임의의 값 저장
-		// 총점(int), 평균(double), 학점(char) 변수에 값 저장
-		String name = "도연";
-		int kor = 100;
-		int eng = 50;
-		int math = 70;
+		// 국어, 영어, 수학 점수는 45~100 사이의 난수를 발생해서 저장
+		// 경우의 수 = ((최대값 - 최소값 + 1)) + 최소값
+		// A: 90~100 / B: 80~89 / C: 70~79 / D: 60~69 / F: 69 미만(디폴트)
+		int kor = (int)(Math.random() * 56) + 45;
+		int eng = (int)(Math.random() * 56) + 45;
+		int math = (int)(Math.random() * 56) + 45;
+		char grade;
 		
-		int sum = (kor + eng + math);
-		double avg = (kor + eng + math) / 3;
-		// char grade = 'A';
-		
-		System.out.print("이름: " + name + ", ");
-		System.out.print("국어 점수: " + kor + ", ");
-		System.out.print("영어 점수: " + eng + ", ");
-		System.out.print("총점: " + sum + ", ");
-		System.out.print("평균: " + avg);
-		// System.out.print(", " + "학점 : " + grade);
+		switch() {
+			case : 
+				grade = 'A';
+				break;
+				
+			case :
+				grade = 'B';
+				break;
+			
+			case :
+				break;
+				
+			default:
+				grade = 'F';
+				break;
+		}
+		System.out.println("결과: " + grade);
+		System.out.println("국어: " + kor);
+		System.out.println("영어: " + eng);
+		System.out.println("수학: " + math);
 	}
-
 }
