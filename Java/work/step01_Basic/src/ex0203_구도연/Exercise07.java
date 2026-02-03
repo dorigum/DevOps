850c5c6520b82eecb6bf6af500d94133e62b3634
dorigum <41736468+dorigum@users.noreply.github.com>
2026-02-04 00:55:15 +0900

    Update Exercise07.java

--- Java/work/step01_Basic/src/ex0203_구도연/Exercise07.java
+++ Java/work/step01_Basic/src/ex0203_구도연/Exercise07.java
@@ -7,9 +7,17 @@ public class Exercise07 {
 		// 나오는 눈을 (눈1, 눈2) 형태로 출력
 		// 눈의 합이 5가 아니면 -> 계속 주사위를 던짐
 		// 눈의 합이 5라면 -> 멈춤
-		
-//		int dice1 = Math.random();
-//		int dice2 = Math.random();
+		while(true) {
+			// 1~6 범위의 난수 생성
+			// (Math.random() * 경우의 수 + 1) 
+			int dice1 = (int)(Math.random() * 6) + 1;
+			int dice2 = (int)(Math.random() * 6) + 1;
+			
+			System.out.println("(" + dice1 + ", " + dice2 + ")");
+			
+			if((dice1 + dice2) == 5) {
+				break;
+			}
+		}
 	}
-
 }
