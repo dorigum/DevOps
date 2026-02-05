cde453d5d7c19035feab355b297e2ad0774fe448
dorigum <41736468+dorigum@users.noreply.github.com>
2026-02-05 21:38:24 +0900

    260205 주석 수정

--- Java/work/step01_Basic/src/ex0205_구도연/ArrayEx01.java
+++ Java/work/step01_Basic/src/ex0205_구도연/ArrayEx01.java
@@ -4,7 +4,7 @@ public class ArrayEx01 {
 
 	public static void main(String[] args) {
 		// [Chapter 05] p201 6번
-		// 배열 길어 출력하기
+		// 배열 길이 출력하기
 		int[][] array = {
 			{95, 86},
 			{83, 92, 96},
@@ -15,3 +15,4 @@ public class ArrayEx01 {
 		System.out.println(array[2].length); // 5(인덱스 번호2: 3번째 배열의 열 개수)
 	}
 }
+
