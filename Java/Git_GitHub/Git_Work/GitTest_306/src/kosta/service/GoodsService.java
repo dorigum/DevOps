bc5ad639e2c6e611777e67d3ac516bb2cb3cba6a
Polar bear 빼꼼 <41736468+dorigum@users.noreply.github.com>
2026-02-11 19:36:03 +0900

    260209 Git 실습

---
+++ Java/Git_GitHub/Git_Work/GitTest_306/src/kosta/service/GoodsService.java
@@ -0,0 +1,25 @@
+package kosta.service;
+
+public class GoodsService {
+	public void insert() {
+		System.out.println("insert");
+	}
+	
+	public void select() {
+		System.out.print("select");
+		System.out.println("시나리오 3번 test");
+	}
+	
+	public void test() {
+		System.out.println("강의장에서 수정");
+	}
+	
+	public void update() {
+		System.out.println("local에서 update 수정");
+	}
+	
+	public void delete() {
+		System.out.println("remote에서 delete 수정");
+
+	}
+}
---
+++ Java/Git_GitHub/Git_Work/GitTest_306/src/kosta/view/MainView.java
@@ -0,0 +1,12 @@
+package kosta.view;
+
+public class MainView {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+		System.out.println("테스트 중");
+		
+		System.out.println("메인에 수정함~~");
+	}
+
+}
