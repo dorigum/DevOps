68f5033b4b213732cd6f50f9f5905811f3811eb3
Polar bear 빼꼼 <kkamang03@gmail.com>
2026-02-24 17:44:38 +0900

    260223 테스트 java 파일

---
+++ Java/HelloTest2.java
@@ -0,0 +1,9 @@
+class HelloTest {
+
+   // 시작을 위한 메인 메소드 작성
+   public static void main(String[] args) {
+         // 화면에 출력
+         System.out.println("안녕하세요.");
+         System.out.print("반갑습니다.");
+   }
+}
